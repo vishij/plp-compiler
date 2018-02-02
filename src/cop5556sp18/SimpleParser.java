8754a1cd6e6ef17b0765581d3cecadb03c6124d0
vishij <jvishi39@gmail.com>
2018-02-02 12:14:23 -0500

    parser initial code

---
+++ src/cop5556sp18/SimpleParser.java
@@ -0,0 +1,148 @@
+package cop5556sp18;
+/* *
+ * Initial code for SimpleParser for the class project in COP5556 Programming Language Principles 
+ * at the University of Florida, Spring 2018.
+ * 
+ * This software is solely for the educational benefit of students 
+ * enrolled in the course during the Spring 2018 semester.  
+ * 
+ * This software, and any software derived from it,  may not be shared with others or posted to public web sites,
+ * either during the course or afterwards.
+ * 
+ *  @Beverly A. Sanders, 2018
+ */
+
+
+import static cop5556sp18.Scanner.Kind.*;
+
+import cop5556sp18.Scanner.Kind;
+import cop5556sp18.Scanner.Token;
+
+
+public class SimpleParser {
+	
+	@SuppressWarnings("serial")
+	public static class SyntaxException extends Exception {
+		Token t;
+
+		public SyntaxException(Token t, String message) {
+			super(message);
+			this.t = t;
+		}
+
+	}
+
+
+
+	Scanner scanner;
+	Token t;
+
+	SimpleParser(Scanner scanner) {
+		this.scanner = scanner;
+		t = scanner.nextToken();
+	}
+
+
+	public void parse() throws SyntaxException {
+		program();
+		matchEOF();
+	}
+
+	/*
+	 * Program ::= Identifier Block
+	 */
+	public void program() throws SyntaxException {
+		match(IDENTIFIER);
+		block();
+	}
+	
+	/*
+	 * Block ::=  { (  (Declaration | Statement) ; )* }
+	 */
+	
+	Kind[] firstDec = { KW_int, KW_boolean, KW_image, KW_float, KW_filename };
+	Kind[] firstStatement = {/* TODO  correct this */  };
+
+	public void block() throws SyntaxException {
+		match(LBRACE);
+		while (isKind(firstDec)|isKind(firstStatement)) {
+	     if (isKind(firstDec)) {
+			declaration();
+		} else if (isKind(firstStatement)) {
+			statement();
+		}
+			match(SEMI);
+		}
+		match(RBRACE);
+
+	}
+	
+	public void declaration() {
+		//TODO
+		throw new UnsupportedOperationException();
+	}
+	
+	public void statement() {
+		//TODO
+		throw new UnsupportedOperationException();
+	}
+
+	protected boolean isKind(Kind kind) {
+		return t.kind == kind;
+	}
+
+	protected boolean isKind(Kind... kinds) {
+		for (Kind k : kinds) {
+			if (k == t.kind)
+				return true;
+		}
+		return false;
+	}
+
+
+	/**
+	 * Precondition: kind != EOF
+	 * 
+	 * @param kind
+	 * @return
+	 * @throws SyntaxException
+	 */
+	private Token match(Kind kind) throws SyntaxException {
+		Token tmp = t;
+		if (isKind(kind)) {
+			consume();
+			return tmp;
+		}
+		throw new SyntaxException(t,"Syntax Error"); //TODO  give a better error message!
+	}
+
+
+	private Token consume() throws SyntaxException {
+		Token tmp = t;
+		if (isKind( EOF)) {
+			throw new SyntaxException(t,"Syntax Error"); //TODO  give a better error message!  
+			//Note that EOF should be matched by the matchEOF method which is called only in parse().  
+			//Anywhere else is an error. */
+		}
+		t = scanner.nextToken();
+		return tmp;
+	}
+
+
+	/**
+	 * Only for check at end of program. Does not "consume" EOF so no attempt to get
+	 * nonexistent next Token.
+	 * 
+	 * @return
+	 * @throws SyntaxException
+	 */
+	private Token matchEOF() throws SyntaxException {
+		if (isKind(EOF)) {
+			return t;
+		}
+		throw new SyntaxException(t,"Syntax Error"); //TODO  give a better error message!
+	}
+	
+
+}
+
---
+++ src/cop5556sp18/SimpleParserTest.java
@@ -0,0 +1,92 @@
+ /**
+ * JUunit tests for the Parser for the class project in COP5556 Programming Language Principles 
+ * at the University of Florida, Spring 2018.
+ * 
+ * This software is solely for the educational benefit of students 
+ * enrolled in the course during the Spring 2018 semester.  
+ * 
+ * This software, and any software derived from it,  may not be shared with others or posted to public web sites,
+ * either during the course or afterwards.
+ * 
+ *  @Beverly A. Sanders, 2018
+ */
+
+package cop5556sp18;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import cop5556sp18.Scanner.LexicalException;
+import cop5556sp18.SimpleParser.SyntaxException;
+
+public class SimpleParserTest {
+
+	//set Junit to be able to catch exceptions
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	
+	//To make it easy to print objects and turn this output on and off
+	static final boolean doPrint = true;
+	private void show(Object input) {
+		if (doPrint) {
+			System.out.println(input.toString());
+		}
+	}
+
+
+	//creates and returns a parser for the given input.
+	private SimpleParser makeParser(String input) throws LexicalException {
+		show(input);        //Display the input 
+		Scanner scanner = new Scanner(input).scan();  //Create a Scanner and initialize it
+		show(scanner);   //Display the Scanner
+		SimpleParser parser = new SimpleParser(scanner);
+		return parser;
+	}
+	
+	
+
+	/**
+	 * Simple test case with an empty program.  This throws an exception 
+	 * because it lacks an identifier and a block. The test case passes because
+	 * it expects an exception
+	 *  
+	 * @throws LexicalException
+	 * @throws SyntaxException 
+	 */
+	@Test
+	public void testEmpty() throws LexicalException, SyntaxException {
+		String input = "";  //The input is the empty string.  
+		SimpleParser parser = makeParser(input);
+		thrown.expect(SyntaxException.class);
+		parser.parse();
+	}
+	
+	/**
+	 * Smallest legal program.
+	 *   
+	 * @throws LexicalException
+	 * @throws SyntaxException 
+	 */
+	@Test
+	public void testSmallest() throws LexicalException, SyntaxException {
+		String input = "b{}";  
+		SimpleParser parser = makeParser(input);
+		parser.parse();
+	}	
+	
+	
+	//This test should pass in your complete parser.  It will fail in the starter code.
+	//Of course, you would want a better error message. 
+	@Test
+	public void testDec0() throws LexicalException, SyntaxException {
+		String input = "b{int c;}";
+		SimpleParser parser = makeParser(input);
+		parser.parse();
+	}
+	
+
+}
+	
+
