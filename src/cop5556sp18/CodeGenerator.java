e7b5eeffea770591cd3161308680a73cccc69c80
vishij <jvishi39@gmail.com>
2018-04-02 01:56:08 -0400

    assignment 5 - base code

---
+++ src/cop5556sp18/CodeGenerator.java
@@ -0,0 +1,424 @@
+/**
+ * Starter code for CodeGenerator.java used n the class project in COP5556 Programming Language Principles 
+ * at the University of Florida, Spring 2018.
+ * 
+ * This software is solely for the educational benefit of students 
+ * enrolled in the course during the Spring 2018 semester.  
+ * 
+ * This software, and any software derived from it,  may not be shared with others or posted to public web sites,
+ * either during the course or afterwards.
+ * 
+ *  @Beverly A. Sanders, 2018
+ */
+
+
+package cop5556sp18;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import cop5556sp18.Types.Type;
+import cop5556sp18.AST.ASTNode;
+import cop5556sp18.AST.ASTVisitor;
+import cop5556sp18.AST.Block;
+import cop5556sp18.AST.Declaration;
+import cop5556sp18.AST.ExpressionBinary;
+import cop5556sp18.AST.ExpressionBooleanLiteral;
+import cop5556sp18.AST.ExpressionConditional;
+import cop5556sp18.AST.ExpressionFloatLiteral;
+import cop5556sp18.AST.ExpressionFunctionAppWithExpressionArg;
+import cop5556sp18.AST.ExpressionFunctionAppWithPixel;
+import cop5556sp18.AST.ExpressionIdent;
+import cop5556sp18.AST.ExpressionIntegerLiteral;
+import cop5556sp18.AST.ExpressionPixel;
+import cop5556sp18.AST.ExpressionPixelConstructor;
+import cop5556sp18.AST.ExpressionPredefinedName;
+import cop5556sp18.AST.ExpressionUnary;
+import cop5556sp18.AST.LHSIdent;
+import cop5556sp18.AST.LHSPixel;
+import cop5556sp18.AST.LHSSample;
+import cop5556sp18.AST.PixelSelector;
+import cop5556sp18.AST.Program;
+import cop5556sp18.AST.StatementAssign;
+import cop5556sp18.AST.StatementIf;
+import cop5556sp18.AST.StatementInput;
+import cop5556sp18.AST.StatementShow;
+import cop5556sp18.AST.StatementSleep;
+import cop5556sp18.AST.StatementWhile;
+import cop5556sp18.AST.StatementWrite;
+
+import cop5556sp18.CodeGenUtils;
+
+public class CodeGenerator implements ASTVisitor, Opcodes {
+
+	/**
+	 * All methods and variable static.
+	 */
+
+	static final int Z = 255;
+
+	ClassWriter cw;
+	String className;
+	String classDesc;
+	String sourceFileName;
+
+	MethodVisitor mv; // visitor of method currently under construction
+
+	/** Indicates whether genPrint and genPrintTOS should generate code. */
+	final boolean DEVEL;
+	final boolean GRADE;
+
+	final int defaultWidth;
+	final int defaultHeight;
+	// final boolean itf = false;
+	
+	private Integer slotNumCounter;
+	/**
+	 * @param DEVEL
+	 *            used as parameter to genPrint and genPrintTOS
+	 * @param GRADE
+	 *            used as parameter to genPrint and genPrintTOS
+	 * @param sourceFileName
+	 *            name of source file, may be null.
+	 * @param defaultWidth
+	 *            default width of images
+	 * @param defaultHeight
+	 *            default height of images
+	 */
+	public CodeGenerator(boolean DEVEL, boolean GRADE, String sourceFileName,
+			int defaultWidth, int defaultHeight) {
+		super();
+		this.DEVEL = DEVEL;
+		this.GRADE = GRADE;
+		this.sourceFileName = sourceFileName;
+		this.defaultWidth = defaultWidth;
+		this.defaultHeight = defaultHeight;
+		this.slotNumCounter = 1;
+	}
+
+	@Override
+	public Object visitBlock(Block block, Object arg) throws Exception {
+		for (ASTNode node : block.decsOrStatements) {
+			Label startBlockLabel = new Label();
+			mv.visitLabel(startBlockLabel);
+			node.visit(this, arg);
+		}
+		return null;
+	}
+
+	@Override
+	public Object visitBooleanLiteral(
+			ExpressionBooleanLiteral expressionBooleanLiteral, Object arg)
+			throws Exception {
+		mv.visitLdcInsn(expressionBooleanLiteral.value);
+		return null;
+	}
+
+	@Override
+	public Object visitDeclaration(Declaration declaration, Object arg)
+			throws Exception {
+		declaration.slotNumber = slotNumCounter;
+		slotNumCounter++;
+		Label decLabel = new Label();
+		if(Types.getType(declaration.type) == Type.IMAGE) {
+			mv.visitLabel(decLabel);
+			if(declaration.width == null && declaration.height == null) {
+				mv.visitLdcInsn(defaultWidth);
+				mv.visitLdcInsn(defaultHeight);
+				mv.visitMethodInsn(INVOKESTATIC, RuntimeImageSupport.className, "makeImage", RuntimeImageSupport.makeImageSig, false);
+			} else {
+				declaration.width.visit(this, arg);
+				declaration.height.visit(this, arg);
+				mv.visitMethodInsn(INVOKESTATIC, RuntimeImageSupport.className, "makeImage", RuntimeImageSupport.makeImageSig, false);
+			}
+            mv.visitVarInsn(ASTORE, declaration.slotNumber);
+		} else {
+			mv.visitInsn(ACONST_NULL);
+            mv.visitVarInsn(ASTORE, declaration.slotNumber);
+		}
+		return null;
+	}
+
+	@Override
+	public Object visitExpressionBinary(ExpressionBinary expressionBinary,
+			Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionConditional(
+			ExpressionConditional expressionConditional, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionFloatLiteral(
+			ExpressionFloatLiteral expressionFloatLiteral, Object arg)
+			throws Exception {
+		mv.visitLdcInsn(expressionFloatLiteral.value);
+		return null;
+	}
+
+	@Override
+	public Object visitExpressionFunctionAppWithExpressionArg(
+			ExpressionFunctionAppWithExpressionArg expressionFunctionAppWithExpressionArg,
+			Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionFunctionAppWithPixel(
+			ExpressionFunctionAppWithPixel expressionFunctionAppWithPixel,
+			Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionIdent(ExpressionIdent expressionIdent,
+			Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionIntegerLiteral(
+			ExpressionIntegerLiteral expressionIntegerLiteral, Object arg)
+			throws Exception {
+		// This one is all done!
+		mv.visitLdcInsn(expressionIntegerLiteral.value);
+		return null;
+	}
+
+	@Override
+	public Object visitExpressionPixel(ExpressionPixel expressionPixel,
+			Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionPixelConstructor(
+			ExpressionPixelConstructor expressionPixelConstructor, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionPredefinedName(
+			ExpressionPredefinedName expressionPredefinedName, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitExpressionUnary(ExpressionUnary expressionUnary,
+			Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitLHSIdent(LHSIdent lhsIdent, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitLHSPixel(LHSPixel lhsPixel, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitLHSSample(LHSSample lhsSample, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitPixelSelector(PixelSelector pixelSelector, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitProgram(Program program, Object arg) throws Exception {
+		// TODO refactor and extend as necessary
+		cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+		// cw = new ClassWriter(0); //If the call to mv.visitMaxs(1, 1) crashes,
+		// it is
+		// sometime helpful to
+		// temporarily run it without COMPUTE_FRAMES. You probably
+		// won't get a completely correct classfile, but
+		// you will be able to see the code that was
+		// generated.
+		className = program.progName;
+		classDesc = "L" + className + ";";
+		String sourceFileName = (String) arg;
+		cw.visit(52, ACC_PUBLIC + ACC_SUPER, className, null,
+				"java/lang/Object", null);
+		cw.visitSource(sourceFileName, null);
+
+		// create main method
+		mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main",
+				"([Ljava/lang/String;)V", null, null);
+		// initialize
+		mv.visitCode();
+
+		// add label before first instruction
+		Label mainStart = new Label();
+		mv.visitLabel(mainStart);
+
+		CodeGenUtils.genLog(DEVEL, mv, "entering main");
+
+		program.block.visit(this, arg);
+
+		// generates code to add string to log
+		CodeGenUtils.genLog(DEVEL, mv, "leaving main");
+
+		// adds the required (by the JVM) return statement to main
+		mv.visitInsn(RETURN);
+
+		// adds label at end of code
+		Label mainEnd = new Label();
+		mv.visitLabel(mainEnd);
+		mv.visitLocalVariable("args", "[Ljava/lang/String;", null, mainStart,
+				mainEnd, 0);
+		// Because we use ClassWriter.COMPUTE_FRAMES as a parameter in the
+		// constructor,
+		// asm will calculate this itself and the parameters are ignored.
+		// If you have trouble with failures in this routine, it may be useful
+		// to temporarily change the parameter in the ClassWriter constructor
+		// from COMPUTE_FRAMES to 0.
+		// The generated classfile will not be correct, but you will at least be
+		// able to see what is in it.
+		mv.visitMaxs(0, 0);
+
+		// terminate construction of main method
+		mv.visitEnd();
+
+		// terminate class construction
+		cw.visitEnd();
+
+		// generate classfile as byte array and return
+		return cw.toByteArray();
+	}
+
+	@Override
+	public Object visitStatementAssign(StatementAssign statementAssign,
+			Object arg) throws Exception {
+		
+		return null;
+	}
+
+	@Override
+	public Object visitStatementIf(StatementIf statementIf, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitStatementInput(StatementInput statementInput, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitStatementShow(StatementShow statementShow, Object arg)
+			throws Exception {
+		/**
+		 * TODO refactor and complete implementation.
+		 * 
+		 * For integers, booleans, and floats, generate code to print to
+		 * console. For images, generate code to display in a frame.
+		 * 
+		 * In all cases, invoke CodeGenUtils.genLogTOS(GRADE, mv, type); before
+		 * consuming top of stack.
+		 */
+		statementShow.e.visit(this, arg);
+		Type type = statementShow.e.getType();
+		switch (type) {
+			case INTEGER : {
+				CodeGenUtils.genLogTOS(GRADE, mv, type);
+				mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
+						"Ljava/io/PrintStream;");
+				mv.visitInsn(Opcodes.SWAP);
+				mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream",
+						"println", "(I)V", false);
+			}
+				break;
+			case BOOLEAN : {
+				CodeGenUtils.genLogTOS(GRADE, mv, type);
+				mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
+						"Ljava/io/PrintStream;");
+				mv.visitInsn(Opcodes.SWAP);
+				mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream",
+						"println", "(Z)V", false);
+			}
+			 break;
+			case FLOAT : {
+				CodeGenUtils.genLogTOS(GRADE, mv, type);
+				mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
+						"Ljava/io/PrintStream;");
+				mv.visitInsn(Opcodes.SWAP);
+				mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream",
+						"println", "(F)V", false);
+			}
+			 break;
+			case IMAGE : {
+				CodeGenUtils.genLogTOS(GRADE, mv, type);
+				// TODO implement functionality
+				throw new UnsupportedOperationException();
+			}
+
+		}
+		return null;
+	}
+
+	@Override
+	public Object visitStatementSleep(StatementSleep statementSleep, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitStatementWhile(StatementWhile statementWhile, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object visitStatementWrite(StatementWrite statementWrite, Object arg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+	
+	private String getJVMType(Type type) {
+		if(type.equals(Type.INTEGER))  return "I";
+		if(type.equals(Type.FLOAT))  return "F";
+		if(type.equals(Type.BOOLEAN))  return "Z";
+		if(type.equals(Type.IMAGE))  return "Ljava/awt/image/BufferedImage;";
+		if(type.equals(Type.FILE))  return "Ljava/lang/String;";
+		
+		return "";
+	}
+
+}
